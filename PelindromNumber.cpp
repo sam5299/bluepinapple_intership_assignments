d48908312d747a9d214943309c92bd86b9d5dc0b
unknown <samshinde5299@gmail.com>
2022-01-19 17:55:35 +0530

    updated PelindromNumber

--- PelindromNumber.cpp
+++ PelindromNumber.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 int main(){
     int number = 0;
+    int copyorignal = 0;
+    int reversed_number = 0;
     cout<<"Enter number:";
     cin>>number;
     number = abs(number);
+    copyorignal = number;
     if(number>=0 && number<=9){
         cout<<"number is pelindrom";
         exit(0);
     }
-
-    int reversed_number = 0;
     while(number>0){
         int rem = number%10;
         reversed_number = reversed_number*10+rem;
         number/=10;
     }
-    if(number==reversed_number){
+   
+    if(copyorignal == reversed_number){
         cout<<"Number is pelindrom:";
     }
     else{
